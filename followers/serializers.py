ed88bb9a1d5f492ca630a4588809e0c06dc27fd5
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-10-04 13:21:16 +0000

    added FolloweList and FolloweDetail

---
+++ followers/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Follower
+from django.db import IntegrityError
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    followed_name = serializers.ReadOnlyField(source="followed.username")
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'created_at', 'followed', 'followed_name'
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError():
+            raise serializers.ValidatedError({
+                'detail': 'possible duplicated'
+            })
