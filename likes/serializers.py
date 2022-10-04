54620dad33dcea5b564e5770cd508c1a0a7d40fb
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-10-04 04:59:45 +0000

    added LikeList and LikeDetail

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Like
+from django.db import IntegrityError
 
 
 class LikeSerializer(serializers.ModelSerializer):
@@ -10,3 +11,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'created_at', 'owner', 'post'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError():
+            raise serializers.ValidationError({
+                'detail': 'possible duplicated'
+            })
