96669a5358b4f0210179656374591c874d387cf3
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-10-17 10:09:54 +0000

    changed settings, to try and sort out api issues with react app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,17 +61,20 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('my_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        r"^https://.*\.gitpod\.io$",
     ]
 
 CORS_ALLOWED_CREDENTIALS = True
